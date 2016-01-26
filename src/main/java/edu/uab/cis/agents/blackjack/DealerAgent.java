828e9c245d62c21cc3733252099c8a50c1ef9c13
DaveWeathers <beruay@gmail.com>
2016-01-25 18:34:23 -0600

    Corrected DealerAgent.java, and Added 5 more tests to DealerAgentTest.java

--- src/main/java/edu/uab/cis/agents/blackjack/DealerAgent.java
+++ src/main/java/edu/uab/cis/agents/blackjack/DealerAgent.java
@@ -7,9 +7,71 @@ import java.util.List;
  * more points. Then the dealer takes the {@link Action#STAND} action.
  */
 public class DealerAgent implements Agent {
+private int count;
+private int ace;
 
   @Override
   public Action act(List<Card> cards) {
-    throw new UnsupportedOperationException();
+    count(cards);
+    return decide();
+
+
+    //throw new UnsupportedOperationException();
   }
+
+  private void count(List<Card> cards){
+    count = 0;
+    ace = 0;
+	for(int i = 0; i < cards.size(); i++){
+		switch(cards.get(i).getRank()){
+            case ACE:
+                count += 11;
+                ace += 1;
+                break;
+            case TWO:
+                count += 2;
+                break;
+            case THREE:
+                count += 3;
+                break;
+            case FOUR:
+                count += 4;
+                break;
+            case FIVE:
+                count += 5;
+                break;
+            case SIX:
+                count += 6;
+                break;
+            case SEVEN:
+                count += 7;
+                break;
+            case EIGHT:
+                count += 8;
+                break;
+            case NINE:
+                count += 9;
+                break;
+            default:
+                count += 10;
+		}
+	}
+	return;
+}
+private Action decide(){
+
+  if(count < 17){
+        return Action.HIT;
+    } else if (count > 16 && count < 22){
+        return Action.STAND;
+    } else {
+        if(ace > 0){
+        ace = ace - 1;
+        count = count - 10;
+        return decide();
+        } else {
+            return Action.BUST;
+        }
+    }
+    }
 }
--- src/test/java/edu/uab/cis/agents/blackjack/DealerAgentTest.java
+++ src/test/java/edu/uab/cis/agents/blackjack/DealerAgentTest.java
@@ -30,4 +30,40 @@ public class DealerAgentTest {
     Action action = agent.act(Arrays.asList(d6, cJ));
     Assert.assertEquals(Action.HIT, action);
   }
+    @Test
+  public void testDoubleAces() {
+    DealerAgent agent = new DealerAgent();
+    Card d6 = new Card(Rank.ACE, Suit.DIAMONDS);
+    Card cJ = new Card(Rank.ACE, Suit.CLUBS);
+    Action action = agent.act(Arrays.asList(d6, cJ));
+    Assert.assertEquals(Action.HIT, action);
+  }
+      @Test
+  public void testKingDoubleAces() {
+    DealerAgent agent = new DealerAgent();
+    Card d6 = new Card(Rank.ACE, Suit.DIAMONDS);
+    Card cJ = new Card(Rank.ACE, Suit.CLUBS);
+    Card eK = new Card(Rank.KING, Suit.SPADES);
+    Action action = agent.act(Arrays.asList(d6, cJ, eK));
+    Assert.assertEquals(Action.HIT, action);
+}
+    @Test
+  public void testKingBust() {
+    DealerAgent agent = new DealerAgent();
+    Card d6 = new Card(Rank.FIVE, Suit.DIAMONDS);
+    Card cJ = new Card(Rank.NINE, Suit.CLUBS);
+    Card eK = new Card(Rank.KING, Suit.SPADES);
+    Action action = agent.act(Arrays.asList(d6, cJ, eK));
+    Assert.assertEquals(Action.BUST, action);
+}
+     @Test
+  public void testKingDoubleAcesBust() {
+    DealerAgent agent = new DealerAgent();
+    Card d6 = new Card(Rank.ACE, Suit.DIAMONDS);
+    Card cJ = new Card(Rank.ACE, Suit.CLUBS);
+    Card fO = new Card(Rank.TEN, Suit.CLUBS);
+    Card eK = new Card(Rank.KING, Suit.SPADES);
+    Action action = agent.act(Arrays.asList(d6, cJ, eK, fO));
+    Assert.assertEquals(Action.BUST, action);
+}
 }
